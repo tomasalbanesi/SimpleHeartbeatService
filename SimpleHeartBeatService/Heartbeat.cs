7fd1b6c020a9e22e123b35c5d0c140754d799894
tomas.albanesi <tomas.albanesi2000@gmail.com>
2022-01-10 15:21:18 -0300

    Initial commit

---
+++ SimpleHeartBeatService/Heartbeat.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace SimpleHeartBeatService
+{
+    public class Heartbeat
+    {
+        private readonly Timer _timer;
+
+        public Heartbeat()
+        {
+            _timer = new Timer(1000); // 1000ms = 1s de intervalo
+            _timer.AutoReset = true;
+            _timer.Elapsed += TimerElapsed;
+
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            string[] lines = new string[] { DateTime.Now.ToString() };
+            File.AppendAllLines(@"C:\Users\tomas\Documents\Temp\Demos\Heartbeat.txt", lines);
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
